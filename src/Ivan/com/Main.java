14fbcc435c84828ab5b6cfecb056a64bd8c9536e
Ivan-Alexandrovich <77282266+Ivan-Alexandrovich@users.noreply.github.com>
2021-01-21 11:57:31 +0300

    Update Main.java

--- src/Ivan/com/Main.java
+++ src/Ivan/com/Main.java
@@ -15,7 +15,7 @@ public class Main {
         for (int i = 0; i < array.length; i++) {
             array[i] = (float) (array[i] * Math.sin(0.2F + i / 5) * Math.cos(0.2F + i / 5) * Math.cos(0.4F + i / 2));
         } //пробегаемся по массиву и преобразовываем его элементы по формуле
-        return array; //возвращаем массив float - 32 битный дробный тип
+        return array; 
     }
 
     public static void runOneThread() {
